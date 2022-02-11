2d430af462c843b79e915b03505e9aabccae0933
Oğuz Taşdemir <93481181+oguztasdemir@users.noreply.github.com>
2022-02-11 21:52:44 +0300

    Add files via upload

---
+++ GDSC/ödev.py
@@ -0,0 +1,75 @@
+"""
+#1. soru
+print("... fırından ... tane ekmek; marketten ... tane şeker ve ... litrelik kola aldı.")
+a=input("Kişinin ismi ne olsun")
+b=input("Kaç tane ekmek alsın?")
+c=input("Kaç tane şeker alsın")
+d=input("Kaç litrelik kola alsın")
+
+print(a,"fırından",b,"tane ekmek; marketten ",c," tane şeker ve ",d," litrelik kola aldı.")
+"""
+
+
+"""
+#2. soru
+kütle = int(input("Lütfen cismin kütlesini giriniz: "))
+sürat = int(input("Cismin süratini giriniz: "))
+yükseklik = int(input("Cismin yüksekliğini giriniz: "))
+g = int(9.8)
+
+kinetik = 1/2*kütle*sürat**2
+potansiyel = kütle*g*yükseklik
+
+mekanik = potansiyel + kinetik
+
+print(mekanik)
+"""
+
+"""
+#3. soru
+c = int(input(Celcius derecesini giriniz: ))
+f = (9/5*c) + 32
+print(f)
+"""
+
+"""
+#4. soru
+cokgen = int(input("Dikdörtgen prizması 1, küre 2, koni 3"))
+
+if cokgen == 1:
+    a = int(input("Lütfen kenarlardan birini giriniz: "))
+    b = int(input("Lütfen kenarlardan birini giriniz: "))
+    c = int(input("Lütfen kenarlardan birini giriniz: "))
+    hacim = a*b*c
+    print(hacim)
+    
+elif cokgen == 2:
+    a = int(input("Lütfen yarıçapı giriniz: "))
+    hacim = 4/3*3.14*a**2
+    print(hacim)
+
+
+elif cokgen == 3:
+    a = int(input("Lütfen yarıçapı giriniz: "))
+    hacim = 1/3*3.14*a**2
+    print(hacim)
+"""
+
+
+"""
+#5. soru
+
+g = int(9.8)
+kütle = int(input("Lütfen cismin kütlesini giriniz: "))
+sürat = int(input("Cismin süratini giriniz: "))
+
+yükseklik = (1/2*kütle*sürat**2) / kütle*g
+print(yükseklik)
+"""
+
+
+
+
+
+
+
